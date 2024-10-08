2f967207efe3f9b20baf626552f8ee501f93b7fa
Smruti Ranjan Bhanja <smrutibhanja32@gmail.com>
2024-10-08 09:36:22 +0000

    Add MapView

---
+++ frontend/src/shared/components/UIElements/MapView.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect } from "react";
+import "ol/css";
+import Map from "ol/Map";
+import View from "ol/View";
+import { fromLonLat } from "ol/proj";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
+import "./MapView.css";
+
+const MapView = (props) => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    new Map({
+      target: mapRef.current.id,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([center.lng, center.lat]),
+        zoom: zoom,
+      }),
+    });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+      id="map"
+    ></div>
+  );
+};
+
+export default MapView;
